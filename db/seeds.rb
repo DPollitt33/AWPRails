76f50422689538bc3791acced4d6106606fc6f78
Dakota E. Pollitt <pollit33@students.rowan.edu>
2016-06-07 17:42:38 -0400

    Quiz question 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,38 +8,35 @@
 #---
 # encoding: utf-8
 Product.delete_all
-Product.create!(title: 'CoffeeScript',
-  description: 
-    %{<p>
-        CoffeeScript is JavaScript done right. It provides all of JavaScript's
-	functionality wrapped in a cleaner, more succinct syntax. In the first
-	book on this exciting new language, CoffeeScript guru Trevor Burnham
-	shows you how to hold onto all the power and flexibility of JavaScript
-	while writing clearer, cleaner, and safer code.
-      </p>},
-  image_url:   'cs.jpg',    
-  price: 36.00)
-# . . .
-Product.create!(title: 'Programming Ruby 1.9 & 2.0',
+Product.create!(title: 'Dimensional Teleporter Gun',
   description:
     %{<p>
-        Ruby is the fastest growing and most exciting dynamic language
-        out there. If you need to get working programs delivered fast,
-        you should add Ruby to your toolbox.
+        Journey far and wide to all corners of the multiverse! This is the key to
+        finally finding a universe where your family loves you! Be sure to eliminate
+        that dimension's version of yourself! Can't have any paradoxes, now can we?
       </p>},
-  image_url: 'ruby.jpg',
-  price: 49.95)
+  image_url: 'teleport.png',
+  price: 39.95)
 # . . .
-
-Product.create!(title: 'Rails Test Prescriptions',
+Product.create!(title: 'Household Plumbus',
   description: 
     %{<p>
-        <em>Rails Test Prescriptions</em> is a comprehensive guide to testing
-        Rails applications, covering Test-Driven Development from both a
-        theoretical perspective (why to test) and from a practical perspective
-        (how to test effectively). It covers the core Rails testing tools and
-        procedures for Rails 2 and Rails 3, and introduces popular add-ons,
-        including Cucumber, Shoulda, Machinist, Mocha, and Rcov.
+        Everyone has a plumbus in their home! Crafted from "Grade &" Dinglepop and 
+        rubbed with Fleeb juice from Dimension P-492, this tool will handle all your
+        plumbus-y needs... and more *winky-face*.
       </p>},
-  image_url: 'rtp.jpg',
+  image_url:   'plumbus.png',    
+  price: 9.99)
+# . . .
+
+Product.create!(title: 'Meseeks Box',
+  description: 
+    %{<p>
+        Household tasks getting you down? Just don't have time to please your wife?
+        Need someone to create a rails application for you by Tuesday? Ask Mr. Meseeks!
+        Capable of performing a number of tasks before disappearing from existence! Just
+        keep it simple! (Best let the neighbor handle that frigid whore) <br> <br>
+        <em> Warning: Failing to keep tasks simple may result in murderous malfunction </em>
+      </p>},
+  image_url: 'meseeks.png',
   price: 34.95)
